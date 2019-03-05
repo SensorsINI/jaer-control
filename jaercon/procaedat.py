2be049b12d4107eb27ba46970a2eef77a7f5ccaa
Yuhuang Hu <duguyue100@gmail.com>
2019-03-05 14:01:57 +0100

    partially working correlation computation

--- jaercon/procaedat.py
+++ jaercon/procaedat.py
@@ -373,13 +373,13 @@ def load_and_decode_davis_rec(file_path, length=0, verbose=False):
 
     aps_ts = (outputData["timeStampStart"] + outputData["timeStampEnd"])//2
 
-    dvs_tr = find_trigger(events_ts)
-    aps_tr = find_trigger(aps_ts)
+    #  dvs_tr = find_trigger(events_ts)
+    #  aps_tr = find_trigger(aps_ts)
+    #
+    #  if aps_ts[aps_tr] < events_ts[dvs_tr]:
+    #      aps_tr += 1
 
-    if aps_ts[aps_tr] < events_ts[dvs_tr]:
-        aps_tr += 1
-
-    return (events_ts[dvs_tr:]/1e6, event_x_addrs[dvs_tr:], event_y_addrs[dvs_tr:], event_pols[dvs_tr:],
+    return (events_ts/1e6, event_x_addrs, event_y_addrs, event_pols,
             aps_ts/1e6, outputData["samples"])
 
 
@@ -455,7 +455,8 @@ def decode_ams1c(timestamps, addresses, return_type=True):
     timestamps_cochlea = timestamps[cochlea_events_idx]
     addresses_cochlea = addresses[cochlea_events_idx]
 
-    timestamps_cochlea = timestamps_cochlea - timestamps_cochlea[0]
+    # shouldn't subtract the first event since time is not monotonic
+    #  timestamps_cochlea = timestamps_cochlea - timestamps_cochlea[0]
     timestamps_cochlea = timestamps_cochlea.astype(np.float32)/1e6
 
     # decoding addresses to get ear id, on off id and the channel id
@@ -482,10 +483,10 @@ def load_and_decode_ams1c(file_path,
                           verbose=False):
     """Load and decode ams1c."""
     timestamps, addresses = load_das_rec(file_path, max_events, verbose)
-    tr = find_trigger(timestamps)
-    real_timestamps, real_addresses = timestamps[tr:], addresses[tr:]
+    #  tr = find_trigger(timestamps)
+    #  real_timestamps, real_addresses = timestamps[tr:], addresses[tr:]
 
-    return decode_ams1c(real_timestamps, real_addresses, return_type)
+    return decode_ams1c(timestamps, addresses, return_type)
 
 
 def get_type_id(sensor_type, channel=None,
@@ -505,3 +506,210 @@ def find_trigger(x):
     dx = 1e-3
     dy = np.diff(x) / dx
     return np.where(dy > 1e10)[0][-1] + 1
+
+
+def count_windows(timestamps, window_size=0.005, stride_size=0.001):
+    """Calculate number of windows from a time stamp series.
+
+    # Arguments
+    timestamps: numpy.ndarray
+        1-D array that contains timestamps
+    window_size: float
+        moving windows size in secs
+    stride_size: float
+        stride size in secs
+
+    # Returns
+    num_windows: int
+        number of windows in this timestamps array
+    max_time: int
+        the maximum time in this timestamps array (without zeroing)
+    """
+    min_time = np.min(timestamps)
+    max_time = np.max(timestamps)
+    num_windows = int(
+        ((max_time-min_time)//0.001+1-window_size*1e3)//(stride_size*1e3)+1)
+
+    return num_windows, max_time
+
+
+def compute_das_spikerate(timestamps, channels, num_windows,
+                          window_size=0.005, stride_size=0.001,
+                          channels_range=[1, 64]):
+    """Compute DAS spikerate.
+
+    Actually a spike count histogram
+
+    Assume the resolution is 1ms
+
+    # Argument
+    timestamps: numpy.ndarray
+        list of time stamps (stamps in secs)
+    channels: numpy.ndarrary
+        list of channel ids
+    window_size: float
+        sliding windows size, in (secs)
+    stride_size: float
+        stride size, in (secs)
+    channels_range: list
+        with two numbers that specify the range of the selected channels.
+        e.g. [20, 40] selects channels 20 to 40
+
+        The first channel with id=1
+
+    # Returns
+    das_spike_rate: numpy.ndarray
+        size in (num_channels, num_windows)
+    """
+    num_channels = 64
+    das_spike_count = np.zeros((num_windows, num_channels))
+    for t, c in zip(timestamps, channels):
+        das_spike_count[
+            int(max(0, (t-window_size)//stride_size+1)):
+            int(t//stride_size)+1, c] += 1
+    return das_spike_count[:, channels_range[0]-1:channels_range[1]-1].T
+
+
+def compute_dvs_chunk_spikerate(dvs_spike_rate, timestamps,
+                                x_addresses, y_addresses,
+                                chunk_id, chunk_size,
+                                window_size=0.005, stride_size=0.001):
+    """Compute a chunk of DVS spike rate (in-place modification).
+
+    Assume chunk on y-axis only
+
+    # Argument
+    dvs_spike_rate: numpy.ndarray
+        (num_windows, height, width)
+    timestamps: numpy.ndarray
+        list of time stamps (stamps in secs)
+    x_addresses: numpy.ndarray
+        list of x address, assume [0, 239] if DAVIS240
+    y_addresses: numpy.ndarray
+        list of y address, assume [0, 179] if DAVIS240
+    window_size: float
+        sliding windows size, in (secs)
+    stride_size: float
+        stride size, in (secs)
+    channels_range: list
+        with two numbers that specify the range of the selected channels.
+        e.g. [20, 40] selects channels 20 to 40
+
+        The first channel with id=1
+    """
+    for t, x, y in zip(timestamps, x_addresses, y_addresses):
+        dvs_spike_rate[int(max(0, (t-window_size)//stride_size+1)):
+                       int(t//stride_size)+1,
+                       int(y-chunk_id*chunk_size), x] += 1
+
+
+def compute_dvs_spikerate(dvs_spike_rate, timestamps,
+                          x_addresses, y_addresses,
+                          chunk_id, chunk_size,
+                          window_size=0.005, stride_size=0.001):
+    """Compute the entire DVS spikerate tensor.
+    # Argument
+    dvs_spike_rate: numpy.ndarray
+        (num_windows, height, width)
+    timestamps: numpy.ndarray
+        list of time stamps (stamps in secs)
+    x_addresses: numpy.ndarray
+        list of x address, assume [0, 239] if DAVIS240
+    y_addresses: numpy.ndarray
+        list of y address, assume [0, 179] if DAVIS240
+    window_size: float
+        sliding windows size, in (secs)
+    stride_size: float
+        stride size, in (secs)
+    channels_range: list
+        with two numbers that specify the range of the selected channels.
+        e.g. [20, 40] selects channels 20 to 40
+
+        The first channel with id=1
+    """
+    pass
+
+
+def compute_events_chunk_corr(events_corr_mat, dvs_spike_rate_chunk,
+                              das_spike_rate, chunk_id):
+    """Compute a chunk of event correlation (in place modification).
+
+    # Arguments
+    events_corr_mat: numpy.ndarray
+        event correlation matrix
+    dvs_spike_rate_chunk: numpy.ndarray
+        a chunk of DVS spike rate
+    das_spike_rate: numpy.ndarray
+        the DAS spike rate array
+    chunk_id: int
+        the index of chunk
+    """
+    # take shape
+    chunk_shape = dvs_spike_rate_chunk.shape
+    # subtract mean from dvs spike rate chunk
+    dvs_spike_rate_chunk -= np.mean(
+        dvs_spike_rate_chunk, axis=0, keepdims=True)
+    dvs_spike_rate_chunk = dvs_spike_rate_chunk.reshape(
+            chunk_shape[0], -1)
+
+    # calculate correlation
+    cov_chunk = np.dot(
+        das_spike_rate, dvs_spike_rate_chunk).reshape(
+            (das_spike_rate.shape[0],)+chunk_shape[1:])
+
+    std_chunk = np.dot(
+        np.sqrt(np.sum(
+            das_spike_rate**2, axis=1, keepdims=True)),
+        np.sqrt(np.sum(
+            dvs_spike_rate_chunk**2, axis=0, keepdims=True))).reshape(
+                (das_spike_rate.shape[0],)+chunk_shape[1:])+1e-5
+
+    cov_chunk /= std_chunk
+
+    events_corr_mat[
+        :, chunk_id*chunk_shape[1]:(chunk_id+1)*chunk_shape[1],
+        :] = cov_chunk
+
+
+def compute_events_corr(davis_timestamps, x_addresses, y_addresses,
+                        das_spike_rate, num_windows, max_time,
+                        window_size=0.005, stride_size=0.001, num_chunks=9,
+                        height=180, width=240):
+    # chunk size along y axis
+    y_chunk_size = height//num_chunks
+
+    # select signal
+    time_idx = (davis_timestamps <= max_time)
+    davis_timestamps = davis_timestamps[time_idx]
+    x_addresses = x_addresses[time_idx]
+    y_addresses = y_addresses[time_idx]
+
+    # init spike chunk and correlation matrix
+    spike_chunk = np.zeros((num_windows, y_chunk_size, width))
+    corr_mat = np.zeros((das_spike_rate.shape[0], height, width))
+
+    # subtract per-channel mean from das_spike_rate
+    das_spike_rate -= np.mean(das_spike_rate, axis=1, keepdims=True)
+
+    for chunk_id in range(num_chunks):
+        # selecting right portion of data
+        y_idx = np.logical_and((y_addresses >= chunk_id*y_chunk_size),
+                               (y_addresses < (chunk_id+1)*y_chunk_size))
+        temp_time = davis_timestamps[y_idx]
+        temp_x_addrs = x_addresses[y_idx]
+        temp_y_addrs = y_addresses[y_idx]
+
+        # filling up this chunk
+        compute_dvs_chunk_spikerate(
+            spike_chunk, temp_time, temp_x_addrs, temp_y_addrs,
+            chunk_id, y_chunk_size, window_size=window_size,
+            stride_size=stride_size)
+
+        # computing correlation
+        compute_events_chunk_corr(corr_mat, spike_chunk, das_spike_rate,
+                                  chunk_id)
+
+        # restore to empty
+        spike_chunk = np.zeros((num_windows, y_chunk_size, width))
+
+    return corr_mat
---
+++ scripts/test_compute_frame_correlation.py
@@ -0,0 +1,101 @@
+"""Compute correlation per frame.
+
+Author: Yuhuang Hu
+Email : yuhuang.hu@ini.uzh.ch
+"""
+
+from __future__ import print_function, absolute_import
+import os
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from jaercon import procaedat
+
+# data path
+data_path = os.path.join(
+    os.environ["HOME"], "data", "lipreading", "004", "2")
+file_name_base = "bin_blue_with_L_9_again_8"
+#  file_name_base = "bin_white_by_E_2_now_10"
+
+davis_file_path = os.path.join(
+    data_path, file_name_base+"_davis.aedat")
+das_file_path = os.path.join(
+    data_path, file_name_base+"_das.aedat")
+
+# extract DAS events
+ts_das, ch, _, _, _ = procaedat.load_and_decode_ams1c(
+    das_file_path, return_type=False)
+print("[MESSAGE] Loaded DAS data")
+
+# extract DAVIS events
+ts_dvs, x_addrs, y_addrs, _, ts_aps, aps_frames = \
+    procaedat.load_and_decode_davis_rec(davis_file_path, verbose=False)
+
+print("[MESSAGE] Loaded DAVIS data")
+
+# sync
+das_trigger = procaedat.find_trigger((ts_das*1e6).astype(np.uint32))
+dvs_trigger = procaedat.find_trigger((ts_dvs*1e6).astype(np.uint32))
+aps_trigger = procaedat.find_trigger((ts_aps*1e6).astype(np.uint32))
+
+# filter events
+ts_das = ts_das[das_trigger:]
+ch = ch[das_trigger:]
+
+ts_dvs = ts_dvs[dvs_trigger:]
+x_addrs = x_addrs[dvs_trigger:]
+y_addrs = y_addrs[dvs_trigger:]
+
+# select frame
+aps_frames = aps_frames[aps_trigger:]
+ts_aps = ts_aps[aps_trigger:]
+num_frames = aps_frames.shape[0]
+
+# parameters to define resolution
+window_size = 0.005
+stride_size = 0.001
+num_chunks = 9
+
+# plot per frame correlation
+for frame_idx in range(1, num_frames):
+    time_pre = ts_aps[frame_idx-1]
+    time_curr = ts_aps[frame_idx]
+
+    # select das time between frame
+    frame_ts_das_idx = np.logical_and(
+        ts_das > time_pre,
+        ts_das < time_curr)
+    frame_ts_das = ts_das[frame_ts_das_idx]
+    frame_das_chs = ch[frame_ts_das_idx]
+
+    # select dvs time between frame
+    frame_ts_dvs_idx = np.logical_and(
+        ts_dvs > time_pre,
+        ts_dvs < time_curr)
+    frame_ts_dvs = ts_dvs[frame_ts_dvs_idx]
+    frame_x_adds = x_addrs[frame_ts_dvs_idx]
+    frame_y_adds = y_addrs[frame_ts_dvs_idx]
+
+    # calculate correlation
+    frame_num_windows, frame_max_time = procaedat.count_windows(
+        frame_ts_das, window_size=window_size, stride_size=stride_size)
+    frame_das_spike_rate = procaedat.compute_das_spikerate(
+        frame_ts_das, frame_das_chs, frame_num_windows,
+        window_size=window_size, stride_size=stride_size,
+        channels_range=[20, 40])
+
+    frame_corr_mat = procaedat.compute_events_corr(
+        frame_ts_dvs, frame_x_adds, frame_y_adds,
+        frame_das_spike_rate, frame_num_windows,
+        frame_max_time, window_size=window_size,
+        stride_size=stride_size, num_chunks=num_chunks)
+
+    # plotting for correlation map and respective frame
+    plt.figure()
+    plt.subplot(121)
+    sns.heatmap(np.flip(frame_corr_mat.mean(axis=0), axis=0))
+    plt.subplot(122)
+    plt.imshow(aps_frames[frame_idx], cmap="gray")
+    plt.show()
